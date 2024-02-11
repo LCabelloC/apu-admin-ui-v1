b6f7ca6438f004ef66bbffcd17f61de55daed25b
gonzalocasquete <gonzalocasquete2310@gmail.com>
2024-02-11 20:36:43 +0100

    Agrupacion de constructores y conexiones a las vistas

--- src/controller/LoginController.py
+++ src/controller/LoginController.py
@@ -1,51 +1,64 @@
-# Lecturas de CSV
 import csv
-# Codificacion de la contraseña
 import hashlib
+import sys
 
-# Clase para el control del Login Inicial
-# Gonzalo Casquete Rodriguez
-# V0.0
-
-
-class LoginController:
-    # Constructor que asigna la ruta pasada por parametro como fichero de lectura, obtiene la password y la almacena codificada
-    # La ruta deberia ser ../data/password.csv
-    # Param CsvFile
-    @classmethod
-    def __init__(self, CsvFile):
-        with open(CsvFile, newline='', encoding='utf-8') as file:
-            # Crear un objeto lector CSV
-            lector_csv = csv.reader(file)
-
-            # Ignorar la primera fila si contiene encabezados
-            next(lector_csv, None)
-
-            # Obtener la primera fila (suponiendo que solo hay una contraseña)
-            first_row = next(lector_csv, None)
-
-            # Extraer la contraseña de la primera fila
-            password = first_row[0] if first_row else None
-
-            self._EncryptedPassword = password
-            print(password)
-
-    # Metodo que verifica la password introducida y la almacenada en el CSV
-    # Param password
-    def authenticate_user(self, password):
-        hash_algorithm = hashlib.sha256()
-        hash_algorithm.update(password.encode('utf-8'))
-        hashed_password = hash_algorithm.hexdigest()
-
-        print(hashed_password)
-
-        if hashed_password == self._EncryptedPassword:
-            return True
-        else:
-            return False
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.uic import loadUi
+from secrets import compare_digest  # Para comparación segura de cadenas
 
 
-# Testing
-# controller = LoginController("../data/password.csv")
-# contraseña_input = input("Ingrese su contraseña: ")
-# controller.authenticate_user(contraseña_input)
+class LoginController(QMainWindow):
+    # Constructor, inicia ventana inicial
+    def __init__(self):
+        super(LoginController, self).__init__()
+        loadUi('../view/LoginPage.ui', self)
+
+        self.pushButton.clicked.connect(self.authenticate_user)
+        self.MainView = None
+
+    def authenticate_user(self):
+        # Obtenemos campo de la ventana y la codificamos
+        password = self.lineEdit.text()
+
+        # La codificamos
+        hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+        # Obtenemos campo del csv
+        try:
+            with open('../data/password.csv', 'r') as file:
+                # Crear un objeto lector csv
+                lector_csv = csv.reader(file)
+
+                # Ignorar la primera fila si contiene encabezados
+                next(lector_csv, None)
+
+                # Obtener la primera fila (suponiendo que solo hay una contraseña)
+                first_row = next(lector_csv, None)
+
+                # Extraer la contraseña de la primera fila
+                passwordCsv = first_row[0] if first_row else None
+
+            # Verificamos y redirigimos
+            if compare_digest(passwordCsv, hashed_password):
+                self.MainView = MainController()
+                self.MainView.show()
+                self.close()
+            else:
+                print("Contraseña incorrecta.")
+        except FileNotFoundError:
+            print("Archivo CSV no encontrado.")
+        except Exception as e:
+            print(f"Error al leer el archivo CSV: {e}")
+
+
+class MainController(QMainWindow):
+    def __init__(self):
+        super(MainController, self).__init__()
+        loadUi('../view/MainPage.ui', self)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    login_controller = LoginController()
+    login_controller.show()
+    sys.exit(app.exec_())
--- src/controller/ViewController.py
+++
@@ -1,4 +0,0 @@
-# Clase para el control de acciones de la vista inicial
-class ViewController:
-    def __init__(self):
-        test
